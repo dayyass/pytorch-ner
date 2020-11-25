b8ba7a564ed2a5be514df586269c1860833af8b2
Dani El-Ayyass <dayyass@yandex.ru>
2020-11-25 21:41:21 +0300

    add AttentionWithSkipConnectionLayerNorm

--- pytorch_ner/nn_modules/attention.py
+++ pytorch_ner/nn_modules/attention.py
@@ -26,3 +26,32 @@ class MultiheadSelfAttention(nn.Module):
         attn, _ = self.attention(query=x, key=x, value=x)
         attn = attn.transpose(0, 1)  # back to [batch_size, seq_len, emb_dim]
         return attn
+
+
+class AttentionWithSkipConnectionLayerNorm(nn.Module):
+    """
+    Any kind of Self-Attention with skip-connection and LayerNorm.
+    Transformer-like.
+    """
+
+    def __init__(
+            self,
+            attention_layer: nn.Module,
+            layer_norm: nn.Module,
+            use_skip_connection: bool = True,
+    ):
+        super(AttentionWithSkipConnectionLayerNorm, self).__init__()
+        self.attention = attention_layer
+        self.layer_norm = layer_norm
+        self.use_skip_connection = use_skip_connection
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        attn_out = self.attention(x)
+
+        if self.use_skip_connection:
+            layer_norm_input = attn_out + x
+        else:
+            layer_norm_input = attn_out
+
+        layer_norm_out = self.layer_norm(layer_norm_input)
+        return layer_norm_out
