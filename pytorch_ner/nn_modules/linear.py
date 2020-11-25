c04a777ed91f1f1089f8d7126d066416964f4382
Dani El-Ayyass <dayyass@yandex.ru>
2020-11-25 21:14:31 +0300

    add linear head with tests

---
+++ pytorch_ner/nn_modules/linear.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class LinearHead(nn.Module):
+    """
+    Linear layer wrapper.
+    """
+
+    def __init__(self, linear_head: nn.Module):
+        super(LinearHead, self).__init__()
+        self.linear_head = linear_head
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.linear_head(x)
---
+++ tests/test_linear.py
@@ -0,0 +1,28 @@
+import unittest
+import torch
+import torch.nn as nn
+from pytorch_ner.nn_modules.linear import LinearHead
+
+
+embeddings = torch.randn(10, 20, 128)  # [batch_size, seq_len, emb_dim]
+
+# BILUO head
+linear_head = LinearHead(
+    nn.Sequential(
+        nn.Linear(128, 64),
+        nn.LeakyReLU(),
+        nn.Linear(64, 5),
+    ),
+)
+
+
+class TestLinearHead(unittest.TestCase):
+
+    def test_attention_shape(self):
+        self.assertTrue(
+            linear_head(embeddings).size() == torch.Size([10, 20, 5]),
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
