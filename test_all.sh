2b4bb9d335b513a3f024b1407659f7049b806a99
Dani El-Ayyass <dayyass@yandex.ru>
2020-11-26 15:05:18 +0300

    add pre-commit unittests

--- test_all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#python3 -m unittest tests/test_prepare_data.py &&
-#python3 -m unittest tests/test_dataset.py &&
-#python3 -m unittest tests/test_utils.py &&
-python3 -m unittest tests/test_dropout.py &&
-python3 -m unittest tests/test_normalization.py &&
-python3 -m unittest tests/test_rnn.py &&
-python3 -m unittest tests/test_attention.py &&
-python3 -m unittest tests/test_linear.py &&
