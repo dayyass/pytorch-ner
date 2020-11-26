f11b5521fb1ffabd81aa5db0702c52aa4f93c2d7
Dani El-Ayyass <dayyass@yandex.ru>
2020-11-26 10:03:13 +0300

    hotfix

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,16 @@
 import unittest
+from collections import Counter
 from pytorch_ner.prepare_data import prepare_conll_data_format, get_token2idx, get_label2idx
 from pytorch_ner.utils import process_tokens, process_labels
 
 
-token_seq, label_seq = prepare_conll_data_format('conll.txt')
-token2idx = get_token2idx(token_seq)
-label2idx = get_label2idx(label_seq)
+token_seq, label_seq = prepare_conll_data_format('data/conll.txt')
+
+token2cnt = Counter([token for sentence in token_seq for token in sentence])
+label_set = set(label for sentence in label_seq for label in sentence)
+
+token2idx = get_token2idx(token2cnt)
+label2idx = get_label2idx(label_set)
 
 tokens = ['Simple', 'is', 'better', 'than', 'complex', '.']
 labels = ['O', 'O', 'O', 'O', 'O', 'B-Puctuation']
