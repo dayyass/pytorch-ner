be97bf354ff3437e1bf49990d49be26a3f3875b7
Dani El-Ayyass <dayyass@yandex.ru>
2020-11-27 12:55:57 +0300

    add load_glove function

--- pytorch_ner/nn_modules/embedding.py
+++ pytorch_ner/nn_modules/embedding.py
@@ -1,27 +1,87 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from typing import Tuple, Dict
 from gensim.models import KeyedVectors
 
 
-class EmbeddingWord2Vec(nn.Module):
-    """
-    Init embeddings from gensim word2vec KeyedVectors.
-    """
+def load_glove(
+        path: str,
+        add_pad: bool = True,
+        add_unk: bool = True,
+) -> Tuple[Dict[str, int], np.ndarray]:
 
-    def __init__(self, path: str, freeze: bool = True):
-        super(EmbeddingWord2Vec, self).__init__()
-        model = KeyedVectors.load_word2vec_format(path)
+    token2idx = {}
+    token_embeddings = []
 
-        word_embeddings = model.vectors
-        pad_embedding = np.zeros(shape=model.vector_size)
-        unk_embedding = word_embeddings.mean(axis=0)  # TODO: make better unk embedding initialization
-        embedding_matrix = np.vstack([pad_embedding, unk_embedding, word_embeddings])
+    if add_pad:
+        token2idx['<PAD>'] = len(token2idx)
+    if add_unk:
+        token2idx['<UNK>'] = len(token2idx)
 
-        self.embedding = nn.Embedding.from_pretrained(
-            torch.tensor(embedding_matrix),
-            freeze=freeze,
-        )
+    with open(path, mode='r') as fp:
+        for line in fp:
+            token, embedding = line.split(maxsplit=1)
+            embedding = np.array([float(i) for i in embedding.split()])
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.embedding(x)
+            token2idx[token] = len(token2idx)
+            token_embeddings.append(embedding)
+
+    token_embeddings = np.array(token_embeddings)
+
+    if add_unk:
+        unk_embedding = token_embeddings.mean(axis=0)  # TODO: make better unk embedding initialization
+        token_embeddings = np.vstack([unk_embedding, token_embeddings])
+    if add_pad:
+        pad_embedding = np.zeros(shape=token_embeddings.shape[-1])
+        token_embeddings = np.vstack([pad_embedding, token_embeddings])
+
+    return token2idx, token_embeddings
+
+
+# class EmbeddingWord2Vec(nn.Module):
+#     """
+#     Init embeddings from gensim word2vec KeyedVectors.
+#     """
+#
+#     def __init__(self, path: str, freeze: bool = True):
+#         super(EmbeddingWord2Vec, self).__init__()
+#         model = KeyedVectors.load_word2vec_format(path)
+#
+#         word_embeddings = model.vectors
+#         pad_embedding = np.zeros(shape=model.vector_size)
+#         unk_embedding = word_embeddings.mean(axis=0)  # TODO: make better unk embedding initialization
+#         embedding_matrix = np.vstack([pad_embedding, unk_embedding, word_embeddings])
+#
+#         self.embedding = nn.Embedding.from_pretrained(
+#             torch.tensor(embedding_matrix),
+#             freeze=freeze,
+#         )
+#
+#     def forward(self, x: torch.Tensor) -> torch.Tensor:
+#         return self.embedding(x)
+#
+#
+# class EmbeddingGloVe(nn.Module):
+#     """
+#     Init embeddings from glove.
+#     """
+#
+#     def __init__(self, path: str, freeze: bool = True):
+#         super(EmbeddingGloVe, self).__init__()
+#         model = KeyedVectors.load_word2vec_format(path)
+#
+#         word_embeddings = model.vectors
+#         pad_embedding = np.zeros(shape=model.vector_size)
+#         unk_embedding = word_embeddings.mean(axis=0)  # TODO: make better unk embedding initialization
+#         embedding_matrix = np.vstack([pad_embedding, unk_embedding, word_embeddings])
+#
+#         self.embedding = nn.Embedding.from_pretrained(
+#             torch.tensor(embedding_matrix),
+#             freeze=freeze,
+#         )
+#
+#     def forward(self, x: torch.Tensor) -> torch.Tensor:
+#         return self.embedding(x)
+
+# TODO: test add_pad, add_unk
--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -1,45 +1,55 @@
 import unittest
+import numpy as np
 import torch
-from pytorch_ner.nn_modules.embedding import EmbeddingWord2Vec
-
-
-embeddings = torch.randn(10, 20, 128)  # [batch_size, seq_len, emb_dim]
-
-embedding_w2v_freeze = EmbeddingWord2Vec(path='tests/data/word2vec.wv')
-embedding_w2v_fine_tune = EmbeddingWord2Vec(path='tests/data/word2vec.wv', freeze=False)
+from pytorch_ner.nn_modules.embedding import load_glove  # EmbeddingWord2Vec, EmbeddingGloVe
 
 
 class TestEmbedding(unittest.TestCase):
 
-    def test_embedding_shape(self):
-        self.assertTrue(embedding_w2v_freeze.embedding.weight.shape == torch.Size([10, 100]))
-        self.assertTrue(embedding_w2v_fine_tune.embedding.weight.shape == torch.Size([10, 100]))
+    def test_load_glove(self):
+        token2idx, embedding_matrix = load_glove(path='tests/data/glove.txt')
 
-    def test_embedding_requires_grad(self):
-        self.assertFalse(embedding_w2v_freeze.embedding.weight.requires_grad)
-        self.assertTrue(embedding_w2v_fine_tune.embedding.weight.requires_grad)
-
-    def test_embedding_pad(self):
-        pad_embedding = embedding_w2v_freeze(torch.tensor([0]))
+        self.assertEqual(len(token2idx), 10)
+        self.assertEqual(len(token2idx), embedding_matrix.shape[0])
+        self.assertEqual(embedding_matrix.shape[-1], 100)
+        self.assertEqual(token2idx['<PAD>'], 0)
+        self.assertEqual(token2idx['<UNK>'], 1)
         self.assertTrue(
-            torch.equal(pad_embedding, torch.zeros_like(pad_embedding))
+            np.allclose(embedding_matrix[0], np.zeros_like(embedding_matrix[0])),
+        )
+        self.assertTrue(
+            np.allclose(embedding_matrix[1], embedding_matrix[2:].mean(axis=0)),
         )
 
-        pad_embedding = embedding_w2v_fine_tune(torch.tensor([0]))
-        self.assertTrue(
-            torch.equal(pad_embedding, torch.zeros_like(pad_embedding))
-        )
-
-    def test_embedding_unk(self):
-        unk_embedding = embedding_w2v_freeze(torch.tensor([1]))
-        self.assertTrue(
-            torch.allclose(unk_embedding, embedding_w2v_freeze.embedding.weight[2:].mean(dim=0))
-        )
-
-        unk_embedding = embedding_w2v_fine_tune(torch.tensor([1]))
-        self.assertTrue(
-            torch.allclose(unk_embedding, embedding_w2v_fine_tune.embedding.weight[2:].mean(dim=0))
-        )
+    # def test_embedding_shape(self):
+    #     self.assertTrue(embedding_w2v_freeze.embedding.weight.shape == torch.Size([10, 100]))
+    #     self.assertTrue(embedding_w2v_fine_tune.embedding.weight.shape == torch.Size([10, 100]))
+    #
+    # def test_embedding_requires_grad(self):
+    #     self.assertFalse(embedding_w2v_freeze.embedding.weight.requires_grad)
+    #     self.assertTrue(embedding_w2v_fine_tune.embedding.weight.requires_grad)
+    #
+    # def test_embedding_pad(self):
+    #     pad_embedding = embedding_w2v_freeze(torch.tensor([0]))
+    #     self.assertTrue(
+    #         torch.equal(pad_embedding, torch.zeros_like(pad_embedding))
+    #     )
+    #
+    #     pad_embedding = embedding_w2v_fine_tune(torch.tensor([0]))
+    #     self.assertTrue(
+    #         torch.equal(pad_embedding, torch.zeros_like(pad_embedding))
+    #     )
+    #
+    # def test_embedding_unk(self):
+    #     unk_embedding = embedding_w2v_freeze(torch.tensor([1]))
+    #     self.assertTrue(
+    #         torch.allclose(unk_embedding, embedding_w2v_freeze.embedding.weight[2:].mean(dim=0))
+    #     )
+    #
+    #     unk_embedding = embedding_w2v_fine_tune(torch.tensor([1]))
+    #     self.assertTrue(
+    #         torch.allclose(unk_embedding, embedding_w2v_fine_tune.embedding.weight[2:].mean(dim=0))
+    #     )
 
 
 if __name__ == '__main__':
