1472923ab3f5a8be730fdae3fd108b543129247e
Dani El-Ayyass <dayyass@yandex.ru>
2020-12-01 14:10:11 +0300

    fix set_global_seed and add masking

--- pytorch_ner/utils.py
+++ pytorch_ner/utils.py
@@ -30,6 +30,19 @@ def set_global_seed(seed: int):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+def masking(lengths: torch.Tensor) -> torch.BoolTensor:
+    """
+    Convert lengths tensor to binary mask
+    implement: https://stackoverflow.com/questions/53403306/how-to-batch-convert-sentence-lengths-to-masks-in-pytorch
+    """
+
+    lengths_shape = lengths.shape[0]
+    max_len = lengths.max()
+    return torch.arange(end=max_len).expand(size=(lengths_shape, max_len)) < lengths.unsqueeze(1)
 
 
 # TODO: add bio/biluo converters
