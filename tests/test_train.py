8933177f273bd3cdb3150e9b9da0ca9601692e9f
Dani El-Ayyass <dayyass@yandex.ru>
2020-12-02 15:34:09 +0300

    init model in better way and add todo

--- tests/test_train.py
+++ tests/test_train.py
@@ -7,17 +7,11 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
+from tests.test_architecture import model_bilstm as model
+
 from pytorch_ner.prepare_data import prepare_conll_data_format, get_token2idx, get_label2idx
 from pytorch_ner.dataset import NERDataset, NERCollator
 from pytorch_ner.train import train, validate_loop
-
-from pytorch_ner.nn_modules.dropout import SpatialDropout1d
-from pytorch_ner.nn_modules.normalization import LayerNorm
-from pytorch_ner.nn_modules.embedding import Embedding, EmbeddingWithDropout
-from pytorch_ner.nn_modules.rnn import DynamicRNN
-from pytorch_ner.nn_modules.attention import MultiheadSelfAttention, AttentionWithSkipConnectionLayerNorm
-from pytorch_ner.nn_modules.linear import LinearHead
-from pytorch_ner.nn_modules.architecture import BiLSTM
 
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -40,23 +34,7 @@ dataloader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collator
 
 # INIT MODEL
 
-embedding_layer = EmbeddingWithDropout(
-    embedding_layer=Embedding(num_embeddings=len(token2idx), embedding_dim=128),
-    dropout=SpatialDropout1d(p=0.)
-)
-rnn_layer = DynamicRNN(rnn_unit=nn.LSTM, input_size=128, hidden_size=256, num_layers=1, dropout=0., bidirectional=True)
-attention_layer = AttentionWithSkipConnectionLayerNorm(
-    attention_layer=MultiheadSelfAttention(embed_dim=512, num_heads=8, dropout=0.),
-    layer_norm=LayerNorm(normalized_shape=512),
-    use_skip_connection=True,
-)
-linear_head = LinearHead(linear_head=nn.Linear(in_features=512, out_features=len(label2idx)))
-
-model = BiLSTM(
-    embedding_layer=embedding_layer,
-    rnn_layer=rnn_layer,
-    linear_head=linear_head,
-).to(device)
+model.to(device)
 
 
 # CRITERION AND OPTIMIZER
@@ -82,6 +60,7 @@ train(
 
 class TestTrain(unittest.TestCase):
 
+    # TODO: fix it - not always True
     def test_val_metrics(self):
 
         val_metrics = validate_loop(
