70a84c9f9406f46b128452b67c983bc8b3382b04
dvelajya <dvelajya@mts.ru>
2020-12-03 13:25:22 +0300

    fix todo

--- tests/test_onnx.py
+++ tests/test_onnx.py
@@ -1,11 +1,6 @@
-import os
-
 from pytorch_ner.utils import mkdir
 from pytorch_ner.onnx import onnx_export_and_check
 from tests.test_nn_modules.test_architecture import model_bilstm as model
-
-# TODO: fix it
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 
 path_to_save = 'models/model.onnx'
