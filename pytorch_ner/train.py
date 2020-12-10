3375d6eb664ba9f9f3a0db360f3e7ba0745eae75
Abdallah Ahmed <abdallah.wallyallah97@gmail.com>
2020-12-10 16:36:05 +0200

    Solve code format check with isort

--- pytorch_ner/train.py
+++ pytorch_ner/train.py
@@ -6,8 +6,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
 
 from pytorch_ner.metrics import calculate_metrics
 from pytorch_ner.utils import to_numpy
