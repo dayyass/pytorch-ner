3aff05bf3782b5aa5f5a0621d54c7217713ff5e9
Dani El-Ayyass <dayyass@yandex.ru>
2020-12-20 12:15:52 +0300

    fix set_global_seed

--- pytorch_ner/utils.py
+++ pytorch_ner/utils.py
@@ -14,6 +14,7 @@ def set_global_seed(seed: int):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
