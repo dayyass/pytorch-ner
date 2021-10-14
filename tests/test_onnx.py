52919315189968ec3690f0366ba100ee4a0d5d7f
Dani El-Ayyass <dayyass@yandex.ru>
2021-10-14 23:06:39 +0300

    add tear_dowm

--- tests/test_onnx.py
+++ tests/test_onnx.py
@@ -1,6 +1,11 @@
+import os
+
 from pytorch_ner.onnx import onnx_export_and_check
 from tests.test_nn_modules.test_architecture import model_bilstm as model
 
 path_to_save = "models/model.onnx"
 
 onnx_export_and_check(model=model, path_to_save=path_to_save)
+
+# tear down
+os.remove(path_to_save)
